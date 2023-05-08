79da0621cef4c24044710a6e3a784f9a0f090ad5
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-08 22:05:32 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -49,6 +49,14 @@ app.get('/', (req, res) => {
 
   })
 
+  app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('detail', { todo }))
+      .catch(error => console.log(error))
+  })
+
   app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
