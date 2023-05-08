b8f6f53281b1dec35451128474d47e9130cf8efb
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-08 20:52:25 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -23,10 +25,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.get('/', (req,res) => {
-    res.send('hello world')
-})
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+app.get('/', (req, res) => {
+    Todo.find() // 取出 Todo model 裡的所有資料
+      .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+      .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+      .catch(error => console.error(error)) // 錯誤處理
+  })
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
