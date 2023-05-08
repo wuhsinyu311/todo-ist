2bfbe81b5b6f40bdaff8ceb860697553bc311677
wuhsinyu311 <wuhsinyu311@gmail.com>
2023-05-08 18:55:06 +0800

    feat: add todo seeder

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
+
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
+
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
@@ -26,5 +28,5 @@ app.get('/', (req,res) => {
 })
 
 app.listen(3000, () => {
-    console.log('App is running on http://localhost3000')
+    console.log('App is running on http://localhost:3000')
 })
